c26c04653e3dc073b42aae5bc7c0bfd0880928d3
nunddigo <nunddigo@gmail.com>
2026-08-12 14:51:34 +0900

    links 사이트 추가 + links.nunddigo.com 라우팅

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    if (url.hostname === "eyetest.nunddigo.com" && !url.pathname.startsWith("/eyetest")) {
-      url.pathname = "/eyetest" + url.pathname;
-    } else if (url.hostname === "eyefit.nunddigo.com" && !url.pathname.startsWith("/eyefit")) {
-      url.pathname = "/eyefit" + url.pathname;
+    const map = {
+      "eyetest.nunddigo.com": "/eyetest",
+      "eyefit.nunddigo.com": "/eyefit",
+      "links.nunddigo.com": "/links",
+    };
+    const prefix = map[url.hostname];
+    if (prefix && !url.pathname.startsWith(prefix)) {
+      url.pathname = prefix + url.pathname;
     }
     return env.ASSETS.fetch(new Request(url.toString(), request));
   }
